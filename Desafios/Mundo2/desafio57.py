282f3a6076b01be743f64656d4d78067eb6b42be
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-02-12 00:15:30 -0300

    Desafios_Aula14

--- Desafios/Mundo2/desafio57.py
+++ Desafios/Mundo2/desafio57.py
@@ -1,5 +1,42 @@
 """ Faça um programa que leia o SEXO de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a
 digitação novamente até ter um valor correto."""
 
+s = 'M'
+while s:
+    if s == 'M' or s == 'F':
+        s = str(input('Escolha o seu sexo:\n'
+                      '- [ M ] Para sexo MASCULINO\n'
+                      '- [ F ] Para sexo FEMININO\n'
+                      '- Ou aperte ENTER para parar a execução do programa.\n'
+                      'Digite a opção desejada: ')).upper().strip()
+        print('---' * 20)
+    elif s != 'M' or s != 'F':
+        print('\033[1;31mEscolha uma opção válida!\033[m\n')
+        s = str(input('Escolha o seu sexo:\n'
+                      '- [ M ] Para sexo MASCULINO\n'
+                      '- [ F ] Para sexo FEMININO\n'
+                      '- Ou aperte ENTER para parar a execução do programa.\n'
+                      'Digite a opção desejada: ')).upper().strip()
+        print('---' * 20)
+else:
+    print('\033[1;34mVOCÊ OPTOU POR FINALIZAR O PROGRAMA\033[m')
 
-
+# s = 'M'
+# while s:
+#     if s == 'M' or s == 'F':
+#         s = str(input('Escolha o seu sexo:\n'
+#                       '- [ M ] Para sexo MASCULINO\n'
+#                       '- [ F ] Para sexo FEMININO\n'
+#                       '- Ou aperte ENTER para parar a execução do programa.\n'
+#                       'Digite a opção desejada: ')).upper().strip()
+#         print('---' * 20)
+#     elif s != 'M' or s != 'F':
+#         print('Escolha uma opção válida!\n')
+#         s = str(input('Escolha o seu sexo:\n'
+#                       '- [ M ] Para sexo MASCULINO\n'
+#                       '- [ F ] Para sexo FEMININO\n'
+#                       '- Ou aperte ENTER para parar a execução do programa.\n'
+#                       'Digite a opção desejada: ')).upper().strip()
+#         print('---' * 20)
+# else:
+#     print('VOCÊ OPTOU POR FINALIZAR O PROGRAMA')
--- Desafios/Mundo2/desafio58.py
+++ Desafios/Mundo2/desafio58.py
@@ -1,2 +1,28 @@
 """Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um NÚMERO ENTRE 0 E 10. Só que agora o jogador vai
-tentar adivinhar até acertar, mostrando no final quantos palpiter foram necessários para vencer."""
+tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer."""
+
+from random import randint
+from time import sleep
+# print(randint(1, 10))
+
+numero = 0
+while numero <= 100:
+    print(':::' * 30)
+    computador = randint(0, 10)
+    num = int(input('Tente adivinhar o número pensado pelo computador!\n'
+                    '- Escolha um número de 0 a 10: '))
+    if num > 10:
+        print('\n- ERRO AO CONTINUAR, ESCOLHA UM NÚMERO DE 0 A 10!')
+        break
+    if computador != num:
+        numero += 1
+        print(f'- Você pensou no número {num} e o computador pensou no número {computador}, tente novamente!')
+        sleep(0.5)
+    if computador == num:
+        print(':::' * 30)
+        sleep(0.5)
+        print(f'Você escolheu o mesmo número que o computador (número {num}).\n'
+              '- VOCÊ GANHOU!\n'
+              f'- Foram necessárias {numero + 1} tentativas para você acertar!')
+        break
+
