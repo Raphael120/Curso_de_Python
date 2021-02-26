4ea231f65687fc8a6de87da1b14ac5aa4da868de
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-02-25 21:13:00 -0300

    att

--- Desafios/Mundo2/desafio62.py
+++ Desafios/Mundo2/desafio62.py
@@ -1,83 +1,56 @@
 """Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando
 ele disser que quer mostrar 0 TERMOS"""
 
-# termo = int(input('- Insira o primeiro termo da Progressão Aritmética: '))
-# razao = int(input('- Insira a razão da Progressão Aritmética: '))
-# print(':::' * 25)
-#
-# print(f'\033[1m- Os 10 primeiros termos dessa progressão são:\033[m')
-# progressao = -1  # (PTS SIGNIFICA PRIMEIROS TERMOS)
-# # progressao = progressao + 1
-# while progressao < 9:
-#     progressao += 1
-#     PA = termo + razao * progressao
-#     print(PA, end=' -> ')
-# print('FIM')
-# print(':::' * 25)
-# opcao = int(input(f'- A Progressão Aritmética do número {termo} está acima.\n'
-#                   f'Escolha o que deseja fazer agora:\n'
-#                   f'[ 0 ] PARA PARAR O PROGRAMA\n'
-#                   f'[ 1 ] PARA MOSTRAR OUTROS TERMOS\n'
-#                   f'- Digite a opção desejada: '))
-# if opcao == 1:
-#     print(':::' * 25)
-#     print('- Você optou por mostrar outros termos.')
-#     termo1 = int(input('- Insira o primeiro termo da Progressão Aritmética: '))
-#     razao1 = int(input('- Insira a razão da Progressão Aritmética: '))
-#     progressao1 = -1
-#     while progressao1 < 9:
-#         progressao1 += 1
-#         PA = termo1 + razao1 * progressao1
-#         print(PA, end=' -> ')
-#     print('FIM')
-# elif opcao == 0:
-#     print(':::' * 25)
-#     print('- Você optou por parar o programa.')
-# else:
-#     print(':::' * 25)
-#     print('- Você escolheu uma opção inválida.')
-
 PA = 1
-# PAA = 1
-# termoo = 0
-# razaoo = 0
-# resultado = 0
 progressao = -1
 while progressao < 9:
-    termo = int(input('- Insira o primeiro termo da Progressão Aritmética: '))
-    razao = int(input('- Insira a razão da Progressão Aritmética: '))
-    # print(progressao, end=' ')
+    print(':::' * 35)
+    termo = int(input('\033[1m- Insira o primeiro termo da Progressão Aritmética: '))
+    razao = int(input('- Insira a razão da Progressão Aritmética:\033[m'))
+    print(':::' * 35)
     while progressao < 9:
         progressao += 1
-        # termoo = termo
-        # razaoo = razao
         PA = termo + razao * progressao
-        # PAA += PA
-        print(PA, end=' ')
-    while progressao < 10:
-        opcao = int(input(f'\n- Os 10 primeiros termos da Progressão Aritmética estão acima.\n'
-                          f'Escolha o que deseja fazer agora:\n'
+        # print(f'{PA}', end=' --> ')
+        print(f'\033[1;33m{PA}\033[m', end=' --> ')
+    if progressao < 10:
+        print('\n', '===' * 35, sep='')
+        opcao = int(input(f'\033[1m- OS 10 PRIMEIROS TERMOS DA PROGRESSÃO ARITMÉTICA ESTÃO ACIMA.'
+                          f'\nEscolha o que deseja fazer agora:\n'
                           f'[ 0 ] PARA PARAR O PROGRAMA\n'
                           f'[ 1 ] PARA MOSTRAR OUTROS TERMOS\n'
-                          f'- Digite a opção desejada: '))
+                          f'- Digite a opção desejada:\033[m '))
+        print('===' * 35)
         progressao = -1
-        while opcao == 0:
-            print('Você optou por parar o programa.')
-            break
-            # termo = int(input('- Insira o primeiro termo da Progressão Aritmética: '))
-            # razao = int(input('- Insira a razão da Progressão Aritmética: '))
-            # progressao *= 1
-            # while progressao < 9:
-            # progressao += 1
-            # PA = termo + razao * progressao
-            # print(PA)
+        if opcao == 1:
+            print('\033[1;32mVOCÊ OPTOU POR MOSTRAR OUTROS TERMOS.\033[m')
         else:
-            print('Você optou por mostrar outros termos.')
+            print('\033[1;31mVOCÊ OPTOU POR PARAR O PROGRAMA.\033[m')
             break
-    # else:
-    #     print('PROGRAMA FINALIZADO')
-    #     break
-    # print(progressao, end=' ')
-#     # break
-# print(PAA)
-# print(progressao)
+
+# PA = 1
+# progressao = -1
+# while progressao < 9:
+#     print(':::' * 35)
+#     termo = int(input('- Insira o primeiro termo da Progressão Aritmética: '))
+#     razao = int(input('- Insira a razão da Progressão Aritmética:'))
+#     print(':::' * 35)
+#     while progressao < 9:
+#         progressao += 1
+#         PA = termo + razao * progressao
+#         print(f'{PA}', end=' --> ')
+#         # print(f'\033[1m{PA}\033[m', end=' -> ')
+#     if progressao < 10:
+#         print('\n', '===' * 35, sep='')
+#         opcao = int(input(f'- OS 10 PRIMEIROS TERMOS DA PROGRESSÃO ARITMÉTICA ESTÃO ACIMA.'
+#                           f'\nEscolha o que deseja fazer agora:\n'
+#                           f'[ 0 ] PARA PARAR O PROGRAMA\n'
+#                           f'[ 1 ] PARA MOSTRAR OUTROS TERMOS\n'
+#                           f'- Digite a opção desejada: '))
+#         print('===' * 35)
+#         progressao = -1
+#         if opcao == 1:
+#             print('Você optou por mostrar outros termos.')
+#         else:
+#             print('Você optou por parar o programa.')
+#             break
