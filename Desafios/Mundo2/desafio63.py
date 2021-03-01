b17bb947662a8949fb4be4d8fc3ee67b8cec818c
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-03-01 00:47:17 -0300

    .

--- Desafios/Mundo2/desafio63.py
+++ Desafios/Mundo2/desafio63.py
@@ -1,6 +1,8 @@
 """Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma
 SEQUÊNCIA DE FIBONACCI.
 Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8 - 13 - 21 - 34..."""
+'Fórmula: Fn = Fn - 1 + Fn - 2'
+
 
 # fibo = n0, n1, n2
 f = 1
