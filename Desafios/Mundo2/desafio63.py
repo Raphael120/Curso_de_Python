92533e947a5a36af3f46fc318bd63ea3497cc64b
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-03-04 00:07:23 -0300

    .

--- Desafios/Mundo2/desafio63.py
+++ Desafios/Mundo2/desafio63.py
@@ -11,16 +11,19 @@ f = 0
 # f1 = 1
 # f2 = 1
 while f < 9:
-    fn = int(input('Insira um número inteiro para mostrar a sua sequência de Fibonacci: '))
-    fibo = (fn - 1) + (fn - 2)
+    fn1 = int(input('Insira um número inteiro para mostrar a sua sequência de Fibonacci: '))
+    fibo = (fn1 - 1) + (fn1 - 2)
     # fibo2 = fn * (fn + 1) + fn * fn
-    # fn1 = fn + 1
-    # fn2 = fn + fn1
+    fn2 = fn1 + 1
+    fn3 = fn1 + fn2
     # fn3 = fn2 + fn1
     while f < 50:
         # f += 1
-        f += fibo
+        f += fn1
+        f += fn2
+        f += fn3
         print(f, end=' ')
+        # print(fn, fn1, fn2, end=' ')
         # fib = f / fn
         # if fib == 0.61:
             # print(f, end=' ')
@@ -35,4 +38,3 @@ while f < 9:
         # fibo
         # print(n0, n1, n2, end=' ')
         # print(n0, n1, n2, end=' ')
-
