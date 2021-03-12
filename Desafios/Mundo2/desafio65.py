0719dc646ba5ed812843f6477ac75d790fc7d9c9
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-03-11 21:07:53 -0300

    att

--- Desafios/Mundo2/desafio65.py
+++ Desafios/Mundo2/desafio65.py
@@ -2,11 +2,43 @@
 os valores e qual foi o maior e o menor valor lido. O programa deve perguntar ao usuário se ele quer ou não continuar
 a digitar valores."""
 
+# SEM PONTO DE PARARDA (COM 'BREAK')
+
 num = 0
 soma = 0
 media = 0
-maior = 0
-menor = 0
+maior = []
+menor = []
+contagem = 0
+
+while num >= 0:
+    num = int(input("Digite um número inteiro: "))
+    if num >= 0:
+        contagem += 1
+        soma += num
+        media = soma / contagem
+        maior += [num]
+        menor += [num]
+        if num >= 0:
+            print(':::' * 30)
+            opcao = str(input('Você quer digitar outros valores? [S / N] ')).upper().strip()
+            print(':::' * 30)
+            if opcao == 'S':
+                continue
+            else:
+                print('VOCÊ OPTOU POR FINALIZAR O PROGRAMA.\n'
+                      f'- Média dos valores digitados: {media:.1f}.\n'
+                      f'- Maior valor digitado: {max(maior)}.\n'
+                      f'- Menor valor digitado: {min(menor)}.')
+                break
+
+
+# COM PONTO DE PARADA 999(FLAG):
+"""num = 0
+soma = 0
+media = 0
+maior = []
+menor = []
 contagem = 0
 while num != 999:
     num = int(input("Digite um número inteiro (Digite '999' para parar o programa): "))
@@ -14,10 +46,19 @@ while num != 999:
         contagem += 1
         soma += num
         media = soma / contagem
-        maior = num
-        menor = num
-        
+        maior += [num]
+        menor += [num]
+        if num != 999:
+            opcao = str(input('Você quer digitar outros valores? [S / N] ')).upper().strip()
+            if opcao == 'S':
+                continue
+            else:
+                print('VOCÊ OPTOU POR FINALIZAR O PROGRAMA.\n'
+                      f'Média: {media:.1f}\n'
+                      f'Maior: {max(maior)}\n'
+                      f'Menor: {min(menor)}')
+                break
 else:
-    print(f'Média: {media:.1f}\n')
-          # f'Maior: {max(num)}')
-    # maior, menor)
+    print(f'Média: {media:.1f}\n'
+          f'Maior: {max(maior)}\n'
+          f'Menor: {min(menor)}')"""
